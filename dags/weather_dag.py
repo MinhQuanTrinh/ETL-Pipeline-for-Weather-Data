005dc8c10aaacf6aebfed9b75b947c105dfba2d6
Ubuntu <ubuntu@ip-172-31-38-205.ap-southeast-2.compute.internal>
2025-01-28 06:47:15 +0000

    Initial Commit

---
+++ dags/weather_dag.py
@@ -0,0 +1,98 @@
+from airflow import DAG
+from datetime import timedelta, datetime
+from airflow.providers.http.sensors.http import HttpSensor
+import json
+from airflow.providers.http.operators.http import HttpOperator
+from airflow.operators.python import PythonOperator
+import pandas as pd
+
+
+### DATA TRANSFORMATION
+
+def kelvin_to_fahrenheit(temp_in_kelvin):
+    temp_in_fahrenheit = (temp_in_kelvin - 273.15) * (9/5) + 32
+    return temp_in_fahrenheit
+
+def transform_load_data(task_instance):
+    data = task_instance.xcom_pull(task_ids="extract_weather_data")
+    city = data["name"]
+    weather_description = data["weather"][0]['description']
+    temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp"])
+    feels_like_farenheit= kelvin_to_fahrenheit(data["main"]["feels_like"])
+    min_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_min"])
+    max_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_max"])
+    pressure = data["main"]["pressure"]
+    humidity = data["main"]["humidity"]
+    wind_speed = data["wind"]["speed"]
+    time_of_record = datetime.utcfromtimestamp(data['dt'] + data['timezone'])
+    sunrise_time = datetime.utcfromtimestamp(data['sys']['sunrise'] + data['timezone'])
+    sunset_time = datetime.utcfromtimestamp(data['sys']['sunset'] + data['timezone'])
+
+    transformed_data = {"City": city,
+                        "Description": weather_description,
+                        "Temperature (F)": temp_farenheit,
+                        "Feels Like (F)": feels_like_farenheit,
+                        "Minimun Temp (F)":min_temp_farenheit,
+                        "Maximum Temp (F)": max_temp_farenheit,
+                        "Pressure": pressure,
+                        "Humidty": humidity,
+                        "Wind Speed": wind_speed,
+                        "Time of Record": time_of_record,
+                        "Sunrise (Local Time)":sunrise_time,
+                        "Sunset (Local Time)": sunset_time                        
+                        }
+    transformed_data_list = [transformed_data]
+    df_data = pd.DataFrame(transformed_data_list)
+    aws_credentials = {"key": "xxxxxxxxx", "secret": "xxxxxxxxxx", "token": "xxxxxxxxxxxxxx"}
+
+## LOADING INTO S3 BUCKET
+    now = datetime.now()
+    dt_string = now.strftime("%d%m%Y%H%M%S")
+    dt_string = 'current_weather_data_perth_' + dt_string
+    df_data.to_csv(f"{dt_string}.csv")
+    #df_data.to_csv(f"s3://weatherapiairflowyoutubebucket-yml/{dt_string}.csv", index=False, storage_options=aws_credentials)
+
+
+#default arguments
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 8),
+    'email': ['trinhminhquan2810@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG('weather_dag',
+        default_args=default_args,
+        schedule= '@daily',
+        catchup=False) as dag:
+
+        #1st task: check for API availability from OpenWeathermap
+        check_api_ready = HttpSensor(
+        task_id = 'check_api_ready',
+        http_conn_id = 'weathermap_api',
+        endpoint = '/data/2.5/weather?q=Perth&APPID=dd89218a997dc67bbb95545d99c64232'
+        )
+
+## EXTRACTION
+        #2nd task: extract data from api
+        extract_weather_data = HttpOperator(
+        task_id = 'extract_weather_data',
+        http_conn_id = 'weathermap_api',
+        endpoint = '/data/2.5/weather?q=Perth&APPID=dd89218a997dc67bbb95545d99c64232',
+        method = 'GET',
+        response_filter = lambda r: json.loads(r.text),
+        log_response = True
+        )
+
+        #3rd task: tranform load data
+        transform_load_weather_data = PythonOperator(
+            task_id = 'transform_load_weather_data',
+            python_callable=transform_load_data
+        )
+
+        check_api_ready >> extract_weather_data >> transform_load_weather_data
